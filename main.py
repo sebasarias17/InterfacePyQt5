77fb65335b333a25e0b652ea3a532ca8b2775b23
Sebasarias17 <sebasarias33314@gmail.com>
2022-09-05 19:57:01 -0500

    Archivos primera interfaz grafica

--- main.py
+++ main.py
@@ -1,58 +1,96 @@
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QDialog,QApplication,QFileDialog
-from PyQt5.QtGui import QPixmap
+#@autor: Sebastian Arias Usma
 import sys
+from diseño import *
+from PySide2 import QtCore
+from PySide2.QtCore import QPropertyAnimation
+from PySide2 import QtCore,QtGui,QtWidgets
 
-#Iniciamos la app
-app = QtWidgets.QApplication([])
+class MiApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-#Cargamos archivos .ui
-feed = uic.loadUi("Ventana1.ui")
-SeleccionarFoto = uic.loadUi("Ventana2.ui")
-BuscarImagen= uic.loadUi("Ventana3.ui")
+        #Eliminamos la barra de titulo - opaciodad
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setWindowOpacity(1)
 
-def gui_seleccionar():
-    feed.hide()
-    SeleccionarFoto.show()
+        #Realizamos el SizeGrip
+        self.gripSize =10
+        self.grip = QtWidgets.QSizeGrip(self)
+        self.grip.resize(self.gripSize, self.gripSize)
 
-def gui_archivo():
-    fname = QFileDialog.getOpenFileName(caption="Open File",directory="",filter="All Files(*);;CSV Files(*.CSV);;PDF Files(*.pdf)")
-    SeleccionarFoto.lineEdit.setText(fname[0])
+        #Movemos la ventana
+        self.ui.frame_superior.mouseMoveEvent = self.mover_ventana
 
-def gui_foto():
-    feed.hide()
-    BuscarImagen.show()
+        #Acceder a las paginas
+        self.ui.pushButton_6.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.inicio))
+        self.ui.pushButton_7.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.capturar))
+        self.ui.pushButton_8.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.selec_foto))
+        self.ui.pushButton_9.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.selec_archivo))
+        self.ui.pushButton_10.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.ajustes))
 
-def gui_selectfoto():
-    fname = QFileDialog.getOpenFileName(caption="Open File",directory="",filter="All Files(*);;JPG Files(*.jpg);;JPEG Files(*.jpeg);;PNG Files(*.png)")
-    BuscarImagen.pixmap = QPixmap(fname[0])
-    BuscarImagen.label_2.setPixmap(BuscarImagen.pixmap)
+        #Control de la barra de titulos
+        self.ui.btn_segundoplano.clicked.connect(self.admin_btn_segundoplano)
+        self.ui.btn_minimizar.clicked.connect(self.admin_btn_minimizar)
+        self.ui.btn_maximizar.clicked.connect(self.admin_btn_maximizar)
+        self.ui.btn_cerrar.clicked.connect(lambda: self.close())
+        self.ui.btn_minimizar.hide()
 
-def gui_volverArchivo():
-    SeleccionarFoto.hide()
-    feed.show()
+        #menu lateral
+        self.ui.btn_menu.clicked.connect(self.gestionar_menu)
+    
+    def admin_btn_segundoplano(self):
+        self.showMinimized()
 
-def gui_volverImagenes():
-    BuscarImagen.hide()
-    feed.show()
+    def admin_btn_minimizar(self):
+        self.showNormal()
+        self.ui.btn_minimizar.hide()
+        self.ui.btn_maximizar.show()
+    
+    def admin_btn_maximizar(self):
+        self.showMaximized()
+        self.ui.btn_maximizar.hide()
+        self.ui.btn_minimizar.show()
+    
+    def gestionar_menu(self):
+        if True:
+            width = self.ui.frame.width()
+            normal = 0
+            if width == 0:
+                extender = 250
+            else: 
+                extender = normal
+            self.animacion = QPropertyAnimation(self.ui.frame, b'minimumWidth')
+            self.animacion.setDuration(300)
+            self.animacion.setStartValue(width)
+            self.animacion.setEndValue(extender)
+            self.animacion.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animacion.start()
+    
+    ##metdo ResizeGrip
+    def resizeEvent(self, event):
+        rect= self.rect()
+        self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
+    
+    ##metodo mover ventana
+    def  mousePressEvent(self, event):
+        self.clickPosition= event.globalPos()
+    
+    def mover_ventana(self,event):
+        if self.isMaximized == False:
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.clickPosition)
+                self.clickPosition = event.globalPos()
+                event.acept()
+        
+        if event.globalPos().y()<=20:
+            self.showMaximized()
+        else:
+            self.showNormal()
 
-
-
-#Botones
-
-# Botones Feed:
-feed.Archivo.clicked.connect(gui_seleccionar)
-feed.SelextImagen.clicked.connect(gui_foto)
-
-#Botones Seleccionar Archivo
-SeleccionarFoto.pushButton_3.clicked.connect(gui_volverArchivo)
-SeleccionarFoto.SearchButton.clicked.connect(gui_archivo)
-
-#Botones Seleccionar Imagen
-BuscarImagen.SelectButtom.clicked.connect(gui_selectfoto)
-BuscarImagen.back_2.clicked.connect(gui_volverImagenes)
-
-
-#Ejecutable
-feed.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mi_app = MiApp()
+    mi_app.show()
+    sys.exit(app.exec_())
